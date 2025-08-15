461d97eb100b3c692843ae81d523c67c8a089ffd
Dezinn1711 <andreluizrosa4@gmail.com>
2025-08-15 14:51:07 -0300

    Resultado Final - React

--- README.md
+++
@@ -1,2 +0,0 @@
-# todo-List-React
-Minha primeira Todo List em React + Vite // My first Todo List in React + Vite
---
+++ todo/src/components/Todo.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Todo = ({todo, removeTodo, completTodo}) => {
+    return (
+        <div className="todo" style={{textDecoration: todo.isCompleted ? "line-through" : " "}}>
+            <div className="content">
+                <p>{todo.text}</p>
+                <p className="category">({todo.category})</p>
+            </div>
+            <div>
+                <button className='complete' onClick={()=> completTodo(todo.id)}>Completar</button>
+                <button className='remove' onClick={() => removeTodo(todo.id)}>x</button>
+            </div>
+        </div>
+    )
+}
+
+export default Todo
